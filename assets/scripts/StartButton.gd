2910f52c4aea97a0452fb8151f33bb85a04a9929
ShrikeGames <ShrikeGames@gmail.com>
2024-07-02 18:58:31 -0300

    Allow enter and space to start the game

--- assets/scripts/StartButton.gd
+++ assets/scripts/StartButton.gd
@@ -8,5 +8,9 @@ func _ready():
 	text_label.bbcode_enabled = true;
 	text_label.text = "[center]%s[/center]"%[text];
 	
+func _process(delta):
+	if Input.is_action_pressed("accept"):
+		self._on_button_down();
+		
 func _on_button_down():
 	get_tree().change_scene_to_packed(scene_to_change_to);
